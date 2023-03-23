11b7f665d04cf5a679c8c353d9ab63526d39fd66
abhishekrathi16 <rathiabhishek53@gmail.com>
2023-03-23 19:43:48 +0530

    set up password match

--- register.js
+++ register.js
@@ -2,6 +2,8 @@ const togglePassword = document.querySelector("#togglePassword");
 const togglePassword2 = document.querySelector("#togglePassword2");
 const password = document.querySelector(".id_password");
 const password2 = document.querySelector(".id_password2");
+const submitButton = document.querySelector("#submitButton")
+const form = document.querySelector("form")
 
 togglePassword.addEventListener("click", function (e) {
   // toggle the type attribute
@@ -21,22 +23,20 @@ togglePassword2.addEventListener("click", function (e) {
   this.classList.toggle("fa-eye-slash");
 });
 
+function matchPassword(){
+  if(password!=password2){
+    alert("Password and Confirm Password not matched");
+  }else{
+    alert("Password created successfully")
+  }
+}
 
-// const form = document.querySelector("#email")
-// eInput = form.querySelector(".email-input")
-// text = form.querySelector(".etext")
+form.addEventListener("submit",(e)=>{
+  // e.preventDefault();
+  matchPassword();
+})
 
-// form.addEventListener("submit",(e)=>{
-//   e.preventDefault();
-//   let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-//   form.classList.add("error");
-//   form.classList.remove("valid");
-//   if(eInput.value == ""){
-//     text.innerText = "Email can't be blank";
-//   }else if (!eInput.value.match(pattern) ) { //if patter is not matched with user's enter value
-//     text.innerText = "Please enter a valid email";
-//   }else{
-//     form.classList.replace("error" , "valid"); //replacing error class with valid class
-//     text.innerText = "This is a valid email";
-//   }
+// submitButton.addEventListener("click",(e)=>{
+  // e.preventDefault();
+//   matchPassword();
 // })
